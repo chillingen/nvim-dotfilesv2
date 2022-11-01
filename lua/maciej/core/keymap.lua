6bce09671e5d0ec0d686abba077c8659159bee9c
chillingen <chillingen@gmail.com>
2022-11-01 21:14:13 +0100

    comment out keymaps for luasnip

--- lua/maciej/core/keymap.lua
+++ lua/maciej/core/keymap.lua
@@ -28,3 +28,14 @@ keymap.set("n", "<leader>ff", ":Telescope find_files<CR>")
 keymap.set("n", "<leader>fx", ":Telescope live_grep<CR>")
 keymap.set("n", "<leader>fn", ":Telescope buffers<CR>")
 keymap.set("n", "<leader>fp", ":Telescope help_tags<CR>")
+
+-- vim.cmd([[
+-- 	imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+-- 	inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
+-- 
+-- 	snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
+-- 	snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
+-- 
+-- 	imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+-- 	smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+-- ]])
