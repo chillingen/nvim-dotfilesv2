2cc94fe32b2ec4ac655d9e2acbb1c7c4ca0fd355
chillingen <chillingen@gmail.com>
2022-11-01 20:43:50 +0100

    add keymaps for telescope

--- lua/maciej/core/keymap.lua
+++ lua/maciej/core/keymap.lua
@@ -5,10 +5,26 @@ local keymap = vim.keymap
 -- general keymaps
 -- keymap.set("i", "jk", "<ESC>") >> that's a joke lol
 
+-- nnoremap <leader>ff <cmd>Telescope find_files<cr>
+-- nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+-- nnoremap <leader>fb <cmd>Telescope buffers<cr>
+-- nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+
+-- basic needs
+
 keymap.set("n", "<leader>nh", ":nohl<CR>")
 keymap.set("n", "x", "_x")
+
+-- tab control
 
 keymap.set("n", "<leader>to", ":tabnew<CR>")
 keymap.set("n", "<leader>tx", ":tabclose<CR>")
 keymap.set("n", "<leader>tn", ":tabn<CR>")
 keymap.set("n", "<leader>tp", ":tabp<CR>")
+
+-- Telescope binds
+
+keymap.set("n", "<leader>ff", ":Telescope find_files<CR>")
+keymap.set("n", "<leader>fx", ":Telescope live_grep<CR>")
+keymap.set("n", "<leader>fn", ":Telescope buffers<CR>")
+keymap.set("n", "<leader>fp", ":Telescope help_tags<CR>")
