b2c9e6890db8c6cd76225185344af39ddf86d57e
chillingen <chillingen@gmail.com>
2022-11-01 21:51:48 +0100

    remove comments; remove broken keymaps

--- lua/maciej/core/keymap.lua
+++ lua/maciej/core/keymap.lua
@@ -5,15 +5,10 @@ local keymap = vim.keymap
 -- general keymaps
 -- keymap.set("i", "jk", "<ESC>") >> that's a joke lol
 
--- nnoremap <leader>ff <cmd>Telescope find_files<cr>
--- nnoremap <leader>fg <cmd>Telescope live_grep<cr>
--- nnoremap <leader>fb <cmd>Telescope buffers<cr>
--- nnoremap <leader>fh <cmd>Telescope help_tags<cr>
-
 -- basic needs
 
 keymap.set("n", "<leader>nh", ":nohl<CR>")
-keymap.set("n", "x", "_x")
+-- keymap.set("n", "x", "_x")
 
 -- tab control
 
@@ -29,13 +24,3 @@ keymap.set("n", "<leader>fx", ":Telescope live_grep<CR>")
 keymap.set("n", "<leader>fn", ":Telescope buffers<CR>")
 keymap.set("n", "<leader>fp", ":Telescope help_tags<CR>")
 
--- vim.cmd([[
--- 	imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
--- 	inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
--- 
--- 	snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
--- 	snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
--- 
--- 	imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
--- 	smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
--- ]])
