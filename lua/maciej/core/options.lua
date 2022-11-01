d6096d5ce4999b7323fe6bf9b035f6c0a259675d
chillingen <chillingen@gmail.com>
2022-11-01 19:35:07 +0100

    add tab and indentation

--- lua/maciej/core/options.lua
+++ lua/maciej/core/options.lua
@@ -3,3 +3,16 @@ local opt = vim.opt
 -- line numbers
 opt.relativenumber = true
 opt.number = true
+
+-- tab & indentation
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+
+
+-- set cursorcolumn
+-- set cursorline
+-- set mouse=a
+-- set clipboard=unnamed
+-- highlight LineNr ctermfg=darkgrey
+
